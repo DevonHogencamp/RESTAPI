9166d4ac6c94fac48578a0b123d3ba33702f2572
Devon Hogencamp <devhogen1@gmail.com>
2017-03-21 14:57:35 -0700

    setup body parser in app.js

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const express = require('express');
 // Set up our express app
 const app = express();
 
+// Body Parser
+const bodyParser = require('body-parser');
+
 const port = 3000;
+
+// Set up body parser for express
+app.use(bodyParser.json());
 
 // Make express use our routes with /api in front of our routes from routes/api.js
 app.use('/api', require('./routes/api'));
