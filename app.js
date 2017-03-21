44067054eb482c11597e569612f480bd15ba705c
Devon Hogencamp <devhogen1@gmail.com>
2017-03-21 14:45:49 -0700

    made http routes and required them in the app

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 /*
     app.js Main entry point for our app
 */
-
+// Express
 const express = require('express');
 
 // Set up our express app
 const app = express();
 
 const port = 3000;
+
+// Make express use our routes with /api in front of our routes from routes/api.js
+app.use('/api', require('./routes/api'));
 
 // Listen for reqs
 app.listen(port, function () {
--- routes/api.js
+++ routes/api.js
@@ -7,3 +7,36 @@ const express = require('express');
 
 // Express router
 const router = express.Router();
+
+/*  API ROUTES  */
+
+// Get a list of ninjas from the DB
+router.get('/ninjas', function (req, res) {
+    res.send({
+        type: 'GET'
+    });
+});
+
+// Add a new ninja to the DB
+router.post('/ninjas', function (req, res) {
+    res.send({
+        type: 'POST'
+    });
+});
+
+// Update a ninja in the DB
+router.put('/ninjas/:id', function (req, res) {
+    res.send({
+        type: 'PUT'
+    });
+});
+
+// Delete a ninja from the DB
+router.delete('/ninjas/:id', function (req, res) {
+    res.send({
+        type: 'DELETE'
+    });
+});
+
+// Export all of our routes
+module.exports = router;
