a3a0cf67b5e108932a59c996cf3bc6a323ceab4d
Devon Hogencamp <devhogen1@gmail.com>
2017-04-02 18:27:20 -0700

    handled deleting ninjas

--- routes/api.js
+++ routes/api.js
@@ -37,8 +37,10 @@ router.put('/ninjas/:id', function (req, res, next) {
 
 // Delete a ninja from the DB
 router.delete('/ninjas/:id', function (req, res, next) {
-    res.send({
-        type: 'DELETE'
+    Ninja.findByIdAndRemove({
+        _id: req.params.id
+    }).then(function (ninja) {
+        res.send(ninja);
     });
 });
 
